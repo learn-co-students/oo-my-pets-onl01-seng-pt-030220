dddf4039e2a542b116e8c990ebed70da175f7b6c
cdominick091984 <cdominick091984@gmail.com>
2020-04-01 16:37:17 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -85,7 +85,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 
 end
