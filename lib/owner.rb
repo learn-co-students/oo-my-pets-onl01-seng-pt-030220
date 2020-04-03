70836e3962e204e6714859e7de12df5f3d152bab
github username <github email address>
2020-04-02 21:52:10 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
+@@all = []
+
+def self.all
+  @@all
+end
+
 class Owner
-  # code goes here
+  attr_reader :name
+  # attr_accessor :species
+
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def species
+ @name = "human"
+  end
+
+  def say_species
+   @name = "I am a human."
+  end
+
+ 
+
+
 end
