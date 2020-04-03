baeff1111ff733ec5bcc0b538f12370435c5705a
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-03 18:39:07 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
-class Cat
-  # code goes here
-end
+class Cat 
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  def initialize(name, owner = nil)
+    @name = name 
+    @owner = owner 
+    owner.cats << self if owner 
+    @mood = "nervous"
+    save
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+    
+    
+end 
